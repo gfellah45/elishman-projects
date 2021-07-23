2a41542842f2f098c925c21e353c0bbd88c339a1
Jerry Aaron <jerry.aaron45@gmail.com>
2021-07-23 21:56:09 +0100

    site security isuess

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,5 +3,17 @@ import Head from "next/head";
 import Login from "../components/login";
 
 export default function Home() {
-  return <Login />;
+  return (
+    <>
+      <Head>
+        {" "}
+        <title>Report Generator</title>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
+      <Login />
+    </>
+  );
 }
